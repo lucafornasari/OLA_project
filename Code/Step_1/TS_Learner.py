f4f7699900de981c6c284c5550fa2192eaf4b822
Nicolò Fasulo <fasulo.nicol@gmail.com>
2023-06-24 12:05:56 +0200

    step 1 done (missing regret)

--- Code/Step_1/TS_Learner.py
+++ Code/Step_1/TS_Learner.py
@@ -2,19 +2,19 @@ from Learner import *
 
 
 class TS_Learner(Learner):
-    def __init__(self, n_arms):
-        super().__init__(n_arms)  # calling the construction of super class passing the n_arms parameter
-        self.beta_parameters = np.ones((n_arms, 2))  # parameters of Beta distribution
+    def __init__(self, rewards):
+        super().__init__(rewards)  # calling the construction of super class passing the n_arms parameter
+        self.beta_parameters = np.ones((self.n_arms, 2))  # parameters of Beta distribution
 
     def pull_arms(self):
-        idx = np.argmax(np.random.beta(self.beta_parameters[:, 0], self.beta_parameters[:, 1]))
+        idx = np.argmax(np.random.beta(self.beta_parameters[:, 0], self.beta_parameters[:, 1]) * self.rewards)
         # Beta distributions are defined by 2 parameters: alpha and beta
         # Beta distribution draws the probability of each pulled arm
         return idx
-        # 1 - 06 - slide 36 and ongoing
 
-    def update(self, pulled_arms, reward):
+    def update(self, pulled_arm, reward):
         self.t += 1
-        self.update_observations(pulled_arms, reward)
-        self.beta_parameters[pulled_arms, 0] = self.beta_parameters[pulled_arms, 0] + reward
-        self.beta_parameters[pulled_arms, 1] = self.beta_parameters[pulled_arms, 1] + 1.0 - reward
+        b_reward = (1 if reward > 0 else 0)  # binary reward
+        self.update_observations(pulled_arm, b_reward)
+        self.beta_parameters[pulled_arm, 0] += b_reward
+        self.beta_parameters[pulled_arm, 1] += 1.0 - b_reward
--- Code/Step_1/UCB1_Learner.py
+++ Code/Step_1/UCB1_Learner.py
@@ -2,21 +2,24 @@ from Learner import *
 
 
 class UCB1_Learner(Learner):
-    def __init__(self, n_arms):
-        super().__init__(n_arms)
-        self.n_arms = n_arms
-        self.rewards = np.zeros(n_arms)  # cumulative rewards for each arm
-        self.pulls = np.zeros(n_arms)  # number of pulls for each arm
+    def __init__(self, rewards):
+        super().__init__(rewards)
+        self.n_arms = self.n_arms
+        self.cumulative_rewards = np.zeros(self.n_arms)  # cumulative rewards for each arm
+        self.pulls = np.zeros(self.n_arms)  # number of pulls for each arm
 
     def pull_arms(self):
+        if np.any(self.pulls == 0):
+            return np.where(self.pulls == 0)[0][0]
         t = np.sum(self.pulls) + 1
         exploration_bonus = np.sqrt((2 * np.log(t)) / self.pulls)
-        ucb_values = self.rewards / self.pulls + exploration_bonus
+        ucb_values = self.cumulative_rewards / self.pulls + exploration_bonus
         idx = np.argmax(ucb_values)
         return idx
 
-    def update(self, pulled_arms, reward):
+    def update(self, pulled_arm, reward):
         self.t += 1
-        self.update_observations(pulled_arms, reward)
-        self.rewards[pulled_arms] += reward
-        self.pulls[pulled_arms] += 1
+        b_reward = (1 if reward > 0 else 0)  # binary reward
+        self.update_observations(pulled_arm, b_reward)
+        self.cumulative_rewards[pulled_arm] += reward
+        self.pulls[pulled_arm] += 1
